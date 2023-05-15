0107e69f11a874014b5895d0d598bb59d15d931d
william mallak <william.mallak1@gmail.com>
2023-05-15 11:53:05 +0200

    redeploy on render

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -2,11 +2,20 @@ import { useShoppingCart } from '../context/shoppingCartContext'
 import { NavLink } from 'react-router-dom'
 import { useUser } from '../context/userContext'
 import {IoCart} from 'react-icons/io5'
+import { useEffect } from 'react'
 
 export default function Navbar() {
   const {openCloseLoginModal, openCloseSignUpModal, switcher, menu, burger, signOut, isLoggedIn, validateToken} = useUser()
 
-  const {openCloseCart, cartQuantity} = useShoppingCart()
+  const {openCloseCart, cartQuantity} = useShoppingCart()  
+  function refreshData(){
+    return (
+      <a href='https://typescript-shopping-app.onrender.com'></a>
+    )
+  }
+  useEffect(() => {
+    refreshData()
+  }, [])
   
   validateToken()
   
