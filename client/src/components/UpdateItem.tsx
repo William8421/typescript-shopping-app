efbadf3b3b3423764ba12e4c4cd83f7c31e410de
william mallak <william.mallak1@gmail.com>
2023-07-15 14:57:04 +0200

    fixed update item image upload

--- client/src/components/UpdateItem.tsx
+++ client/src/components/UpdateItem.tsx
@@ -1,77 +1,111 @@
 import { useState } from 'react'
-import {ItemsDataProps, SelectedItemProps} from '../types/userTypes'
+import { ItemsDataProps, SelectedItemProps } from '../types/userTypes'
 import { useUser } from '../context/userContext'
+import axios from 'axios'
 
 type UpdateItemProps = {
-    isUpdateItemModalOpen: string
-    openCloseUpdateItemModal: () => void
-    data: ItemsDataProps[]
-    selectedItem: SelectedItemProps
+  isUpdateItemModalOpen: string
+  openCloseUpdateItemModal: () => void
+  data: ItemsDataProps[]
+  selectedItem: SelectedItemProps
 }
 
-export default function UpdateItem({isUpdateItemModalOpen, openCloseUpdateItemModal, data, selectedItem}: UpdateItemProps) {
-  const {isLoggedIn, updateItem} = useUser()
+export default function UpdateItem({ isUpdateItemModalOpen, openCloseUpdateItemModal, data, selectedItem }: UpdateItemProps) {
+  const { isLoggedIn, updateItem } = useUser()
 
   const itemOldData = data.find(item => item.itemId === selectedItem.itemID)
 
-    const [updateItemForm, setUpdateItemForm] = useState({
-        itemName: itemOldData?.itemName,
-        price: itemOldData?.price,
-        imgUrl: itemOldData?.imgUrl
+  const [updateItemForm, setUpdateItemForm] = useState({
+    itemName: itemOldData?.itemName,
+    price: itemOldData?.price,
+    imgUrl: itemOldData?.imgUrl
+  })
+
+  function updateItemFormHandler(e: React.ChangeEvent<HTMLInputElement>) {
+    const element = e.target.name;
+    const value = e.target.value;
+    setUpdateItemForm((prevState) => {
+      return { ...prevState, [element]: value }
     })
+  }
 
-    function updateItemFormHandler(e: React.ChangeEvent<HTMLInputElement>) {
-        const element = e.target.name;
-        const value = e.target.value;
-        setUpdateItemForm((prevState) => {
-          return {...prevState, [element]: value}
-        })
-      }
+  const [imgSelected, setImgSelected] = useState<File | string>('')
+  const [uploaded, setUploaded] = useState('')
 
-      function submit(e: React.FormEvent) {
-        e.preventDefault()
-          if(isLoggedIn){
-    
-            const newItemInfo = {
-              itemId: selectedItem.itemID,
-              itemName: updateItemForm.itemName,
-              price: updateItemForm.price,
-              imgUrl: updateItemForm.imgUrl
-            }
-            updateItem(newItemInfo)
-            
-            setTimeout(() => {
-              window.location.href = '/myprofile'
-            }, 1000)
-        }        
+  function imageHandler(e: any) {
+    setImgSelected(e.target.files[0])
+  }
+
+  const uploadImage = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("file", imgSelected);
+    formData.append("upload_preset", "WilliamMallak");
+    formData.append("upload_name", "denpxdokx");
+    formData.append('folder', 'shopping-cart');
+
+    axios
+      .post(
+        "https://api.cloudinary.com/v1_1/denpxdokx/image/upload",
+        formData
+      )
+      .then((response) => {
+        setUploaded(response.data.secure_url)
+        setUpdateItemForm({ ...updateItemForm, imgUrl: response.data.secure_url })
       }
+      )
+      .catch((err) => {
+        return console.log(err);
+      });
+  };
+
+  function submit(e: React.FormEvent) {
+    e.preventDefault()
+    if (isLoggedIn) {
+
+      const newItemInfo = {
+        itemId: selectedItem.itemID,
+        itemName: updateItemForm.itemName,
+        price: updateItemForm.price,
+        imgUrl: updateItemForm.imgUrl
+      }
+      updateItem(newItemInfo)
+
+      setTimeout(() => {
+        window.location.href = '/myprofile'
+      }, 1000)
+    }
+  }
   return (
     <div>
-       <div className={`update-item-hidden-div ${isUpdateItemModalOpen}`} onClick={openCloseUpdateItemModal}>
+      <div className={`update-item-hidden-div ${isUpdateItemModalOpen}`} onClick={openCloseUpdateItemModal}>
       </div>
-        <div className={`update-item-modal ${isUpdateItemModalOpen}`}>
-            <div className='update-item-modal-header'>
-                <h2>Update Item</h2>
-                <button className='close-button' onClick={openCloseUpdateItemModal}>X</button>
-            </div>
-            <div onChange={updateItemFormHandler} className='update-item-form-container'>
-            <form onSubmit={submit} >
-                <div>
-                    New item Name
-                    <input type='text' name='itemName' />
-                </div>
-                <div>
-                    New Price
-                    <input type='number' name='price' />
-                </div>
-                <div>
-                    New Image
-                    <input type='text' name='imgUrl' />
-                </div>
-                <button className='main-button' type='submit' onClick={openCloseUpdateItemModal}>Save</button>
-            </form>
-            </div>
+      <div className={`update-item-modal ${isUpdateItemModalOpen}`}>
+        <div className='update-item-modal-header'>
+          <h2>Update Item</h2>
+          <button className='close-button' onClick={openCloseUpdateItemModal}>X</button>
         </div>
+        <div onChange={updateItemFormHandler} className='update-item-form-container'>
+          <form onSubmit={submit} >
+            <div>
+              New item Name
+              <input type='text' name='itemName' />
+            </div>
+            <div>
+              New Price
+              <input type='number' name='price' />
+            </div>
+            <div>
+              Image
+              <input type='file' name='imgUrl' onChange={(e) => imageHandler(e)} />
+              {updateItemForm.imgUrl !== '' ? (
+                <button className='main-button' onClick={(e) => uploadImage(e)}>Upload</button>
+              ) : (<div>please upload image</div>)}
+            </div>
+            <button className='main-button' type='submit' onClick={openCloseUpdateItemModal}>Save</button>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
