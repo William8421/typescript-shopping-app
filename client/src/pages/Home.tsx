35f15865e1f87a1917a6df19155ae00617054065
william mallak <william.mallak1@gmail.com>
2023-07-25 17:11:52 +0200

    test new server

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,13 +3,13 @@ import { NavLink } from 'react-router-dom'
 export default function Home() {
   return (
     <div className='home-container'>
-        <div className='home-app-name'>
+      <div className='home-app-name'>
         <h2>Welcome to</h2>
-          <h1>Shopping Cart App</h1>
+        <h1>Shopping Cart App</h1>
         <NavLink className='start-shopping' to='/store'>
-        <h3>Start Shopping</h3>
+          <h3>Start Shopping</h3>
         </NavLink>
-        </div>
+      </div>
     </div>
   )
 }
